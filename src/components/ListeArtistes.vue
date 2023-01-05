7bf5ced4a3f2b9dce82f8b7d1d3d564db64515bc
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-01-05 16:22:33 +0100

    modification en liste de ListeArtistes.vue

--- src/components/ListeArtistes.vue
+++ src/components/ListeArtistes.vue
@@ -1,11 +1,15 @@
 <template>
-  <table>
-    <tr v-for="artiste in listeDesArtistes">
-      <td>{{ artiste.nom }}</td>
-      <td>{{ artiste.id }}</td>
-    </tr>
-  </table>
+  <ul>
+    <li v-for="artiste in listeDesArtistes">
+      <a href="">{{ artiste.nom }}</a>
+    </li>
+  </ul>
 </template>
+<style>
+li {
+  margin: 5px;
+}
+</style>
 <script>
 import axios from "axios";
 export default {
@@ -16,12 +20,14 @@ export default {
     };
   },
   props: {
-    genre: Number,
+    genre: String,
   },
-  mounted() {},
+  async mounted() {
+    await this.recupererListeArtiste();
+  },
   methods: {
     recupererListeArtiste: async function () {
-      await axios.get(`https://api.deezer.com/genre/${genre}/artists`).then((reponse) => {
+      await axios.get(`https://api.deezer.com/genre/${this.genreId}/artists`).then((reponse) => {
         reponse.data.data.forEach((element) => {
           this.listeDesArtistes.push({ nom: element.name, id: element.id });
         });
