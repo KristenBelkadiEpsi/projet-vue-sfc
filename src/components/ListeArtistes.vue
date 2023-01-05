50176915ce9699b37bd27fb7d2712ac736d9f7d0
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-01-05 15:33:13 +0100

    ajout du composant ListeArtistes.vue

---
+++ src/components/ListeArtistes.vue
@@ -0,0 +1,16 @@
+<template>
+  <table>
+    <tr v-for="artiste in listeDesArtistes">
+      <td>{{ artiste.name }}</td>
+      <td>{{ artiste.id }}</td>
+    </tr>
+  </table>
+</template>
+<script>
+export default {
+  name: "ListeArtistes",
+  props: {
+    listeDesArtistes: Array,
+  },
+};
+</script>
