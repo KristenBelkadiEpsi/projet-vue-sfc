a20c9986225bf52fe6bc8f225516eda51a9a15d1
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-01-05 15:49:21 +0100

    finition du composant ListeArtistes.vue

--- src/components/ListeArtistes.vue
+++ src/components/ListeArtistes.vue
@@ -1,16 +1,32 @@
 <template>
   <table>
     <tr v-for="artiste in listeDesArtistes">
-      <td>{{ artiste.name }}</td>
+      <td>{{ artiste.nom }}</td>
       <td>{{ artiste.id }}</td>
     </tr>
   </table>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "ListeArtistes",
+  data() {
+    return {
+      listeDesArtistes: [],
+    };
+  },
   props: {
-    listeDesArtistes: Array,
+    genre: Number,
+  },
+  mounted() {},
+  methods: {
+    recupererListeArtiste: async function () {
+      await axios.get(`https://api.deezer.com/genre/${genre}/artists`).then((reponse) => {
+        reponse.data.data.forEach((element) => {
+          this.listeDesArtistes.push({ nom: element.name, id: element.id });
+        });
+      });
+    },
   },
 };
 </script>
