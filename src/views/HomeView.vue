0767244af3f70675ce0d81c7332b8eaf43619c1e
KristenBelkadiEpsi <kristen.belkadi.etudiant@gmail.com>
2023-01-07 21:25:38 +0100

    modification de HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,19 +6,18 @@
         <h2>Cherche des artistes de ton style préféré</h2>
       </div>
       <div id="inputStyle">
-        <div v-if="listGenre.length > 0" >
-          <ul>
-            
-            <li v-for="genre in listGenre">
-              <router-link :to="'/ListeArtistes/'+genre.id">
-              {{genre.name}}
-            <li><img :src="genre.picture" alt="img" /></li>
+
+        <ul>
+          <li v-for="genre in listGenre">
+            <router-link :to="'/ListeArtistes/' + genre.id">
+              {{ genre.name }}
+          <li><img :src="genre.picture" alt="img" /></li>
           </router-link>
-            </li>
+          </li>
         </ul>
+
       </div>
-      </div>
-     
+
     </div>
   </div>
 </template>
@@ -32,40 +31,41 @@ import axios from 'axios'
 export default {
   name: 'HomeView',
   data() {
-          return {
-            listGenre: [],
-            genre: null,
-          };
-        },
-        
-        created: function() {
-          this.fetchData();
-        },
+    return {
+      listGenre: [],
+      genre: null,
+      apiURL: "https://api.deezer.com/genre"
+    };
+  },
+  created: function () {
+    this.fetchData();
+  },
 
-        watch: {
-          currentGenre: "fetchData",
-        },
+  watch: {
+    currentGenre: "fetchData",
+  },
 
-        methods: {
-          
-          fetchData: function () {
-            const self = this;
-            axios
-              .get(apiURL)
-              .then(function (response) {
-                console.log(response.data, "dataAsked");
-                self.listGenre = response.data.data;
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-          },
-        },
-      }
+  methods: {
+
+    fetchData: function () {
+      const self = this;
+      axios
+        .get(this.apiURL)
+        .then(function (response) {
+          console.log(response.data, "dataAsked");
+          self.listGenre = response.data.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+}
 
 </script>
 <style>
-li{
+li {
   list-style-type: none;
 }
-</script>
+</style>
+
